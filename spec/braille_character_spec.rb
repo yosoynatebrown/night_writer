93cf6f3e9c07ea8a8279f3f76b63f96eaf74ded7
Nathan Brown <nathan.brown263@gmail.com>
2021-09-23 14:28:22 -0600

    Initial commit

---
+++ spec/braille_character_spec.rb
@@ -0,0 +1,15 @@
+require "rspec"
+require "./lib/braille_character"
+
+describe BrailleCharacter do
+  before(:each) do
+  @braille_character = BrailleCharacter.new
+  end
+  it 'exists' do
+  expect(@braille_character).to be_a(BrailleCharacter)
+  end
+
+  it 'attributes' do
+    expect(@braille_character.name).to eq()
+  end
+end
