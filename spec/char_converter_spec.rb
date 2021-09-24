4837c289a6e2e6d2e7f8b20929baba64bf63b8cd
Nathan Brown <nathan.brown263@gmail.com>
2021-09-23 20:18:51 -0600

    Add final hashes to char converter (with tests)

--- spec/char_converter_spec.rb
+++ spec/char_converter_spec.rb
@@ -9,11 +9,23 @@ describe CharConverter do
   expect(@converter).to be_a(CharConverter)
   end
 
+  it '#punctuation_hash' do
+    expect(@converter.punctuation_hash).to be_a(Hash)
+    expect(@converter.punctuation_hash[" "]).to eq("..\n..\n..")
+  end
+
+  it '#lower_case_hash' do
+    expect(@converter.lower_case_hash).to be_a(Hash)
+    expect(@converter.lower_case_hash["h"]).to eq("0.\n00\n..")
+    expect(@converter.lower_case_hash["e"]).to eq("0.\n.0\n..")
+    expect(@converter.lower_case_hash["l"]).to eq("0.\n0.\n0.")
+  end
+
   it "has a braille hash" do
     expect(@converter.braille_hash).to be_a(Hash)
   end
 
-  it "has the right number of characters total" do
+  it "braille hash the right number of characters total" do
     expect(@converter.braille_hash.count).to eq(59)
   end
 end
